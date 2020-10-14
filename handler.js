1a4b143bfa11a5fc7b88cf9ffc5594272b71fc54
Priya <67540583+lcpriya2020@users.noreply.github.com>
2020-10-14 20:51:49 +0400

    Removed user and meeting folders

--- handler.js
+++ handler.js
@@ -52,3 +52,53 @@ module.exports.postmeeting = async (event, context, callback) => {
   
     return response;
   };
+
+  // Post Users data
+module.exports.postusers = async (event, context, callback) => {
+    const db = new AWS.DynamoDB.DocumentClient({ apiVersion: '2012-08-10' });
+    const usersTable = process.env.TABLENAME;
+      
+    let responseBody = '';
+    let statusCode = 0;
+  
+    const data = JSON.parse(event.body);
+    const usersData = {
+      TableName: usersTable,
+      Item: {
+        id: uuidv4(),
+        createdTime: Date.now(),  
+        firstname: data.firstname,
+        lastname: data.lastname,
+        profileurl: data.profileurl,
+        userToken: data.userToken,
+        provider: data.provider,
+        email: data.email,
+        socialId: data.socialId
+      }
+    };
+  
+    try {
+      const data = await db.put(usersData).promise();
+      responseBody = JSON.stringify(data.Items);
+      statusCode = 200;
+    } catch(err) {
+      responseBody = `Unable to create User details ${err}`;
+      statusCode = 403;
+    }    
+    const response = {
+      statusCode,
+      headers: {
+        'Access-Control-Allow-Headers' : 'Content-Type',
+        'Access-Control-Expose-Headers': 'Access-Control-Allow-Origin',
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Credentials': true,
+        'Access-Control-Allow-Methods': 'GET,PUT,POST,DELETE,OPTIONS',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        message: responseBody
+      })
+    };
+  
+    return response;
+  };
